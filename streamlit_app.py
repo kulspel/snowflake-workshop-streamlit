b8c16071b87a523aaa5d052081c0c32880c76175
Björn Pedersen <bjoern.pe@gmail.com>
2022-10-17 10:36:06 +0200

    added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmel')
+streamlit.text('Kale, Spinach & Rocket Smoohie')
+streamlit.text('Hard-Boiled Free-Range Egg')
